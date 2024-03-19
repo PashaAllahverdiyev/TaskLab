7258052753a17d831ebc419078b43b1ba482ef48
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-03-19 11:49:01 +0400

    Add files via upload

---
+++ Lab12/lab12/lab12/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task1
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("eded yaz: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int num2;
+        Console.Write("Ters yazilisi: ");
+        while (num >0)
+        {
+            num2 = num % 10;
+            Console.Write(num2);
+            num /= 10;
+        }
+        
+    }
+}
